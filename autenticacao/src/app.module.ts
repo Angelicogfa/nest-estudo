bfe3bafdb2e806d7873e88638f98374d0270022a
Guilherme Fernando Angelico <guilherme.angelico@s2it.com.br>
2018-10-20 18:47:47 -0300

    Remoção de projeto de autenticacao

--- autenticacao/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from 'auth/auth.module';
-
-@Module({
-  imports: [UsersModule],
-})
-export class AppModule { }
